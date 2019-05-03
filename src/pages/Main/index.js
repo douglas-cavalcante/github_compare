7bf97e51140fdc6262e639a3e8ccb048ce25587d
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-05-03 17:06:34 -0300

    Adiciona chamada Api para repositorios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,16 +2,48 @@ import React from 'react';
 import Logo from '../../assets/logo.png';
 import { Form, Container } from './styles';
 import CompareList from '../../components/CompareList';
+import api from '../../services/api';
 
-const Main = () => (
-  <Container>
-    <img src={Logo} alt="Git Compare" />
-    <Form>
-      <input type="text" placeholder="usuário/repositório" />
-      <button type="submit">Ok</button>
-    </Form>
-    <CompareList />
-  </Container>
-);
+export default class Main extends React.Component {
+  state = {
+    repositoryInput: '',
+    repositories: [],
+  };
 
-export default Main;
+  handleChange = (e) => {
+    this.setState({ repositoryInput: e.target.value });
+  };
+
+  handleAddRepository = async (e) => {
+    const { repositoryInput } = this.state;
+    e.preventDefault();
+    try {
+      const response = await api.get(`repos/${repositoryInput}`);
+      this.setState({
+        repositoryInput: '',
+        repositories: [...this.state.repositories, response.data],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    const { repositoryInput, repositories } = this.state;
+    return (
+      <Container>
+        <img src={Logo} alt="Git Compare" />
+        <Form onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            value={repositoryInput}
+            onChange={this.handleChange}
+            placeholder="usuário/repositório"
+          />
+          <button type="submit">Ok</button>
+        </Form>
+        <CompareList repositories={repositories} />
+      </Container>
+    );
+  }
+}
